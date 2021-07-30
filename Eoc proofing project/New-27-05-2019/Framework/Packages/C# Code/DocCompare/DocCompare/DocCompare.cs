3ca19c9a8347bca79f878072cb4925796cad0435
Sourav <das.sourav53@gmail.com>
2021-07-30 19:20:15 +0530

    1st commit

---
+++ Eoc proofing project/New-27-05-2019/Framework/Packages/C# Code/DocCompare/DocCompare/DocCompare.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data;
+using MsWord = Microsoft.Office.Interop.Word;
+
+namespace DocCompare
+{
+    public class DocCompare : CodeActivity
+    {
+        MsWord.Application wordApp = null;
+        object readOnly = null;
+        object missing = null;
+
+        MsWord.Document doc1 = null;
+        MsWord.Document doc2 = null;
+        MsWord.Document doc = null;
+
+            
+
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<object> FirstFilePath { get; set; }
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<object> SecondFilePath { get; set; }
+        [Category("Input")]
+        //[RequiredArgument]
+        public InArgument<object> OutputPath { get; set; }
+
+
+
+        //[Category("Output")]
+        //public OutArgument<DataTable> Outtable { get; set; }
+        protected override void Execute(CodeActivityContext context)
+        {
+            wordApp = new MsWord.Application();
+            readOnly = true;
+            missing = System.Reflection.Missing.Value;
+
+            doc1 = wordApp.Documents.Open(FirstFilePath.Get(context), missing, readOnly,  missing, missing, missing, missing, missing, missing,
+                        missing, missing, missing, missing, missing, missing, missing);
+            
+            doc2 = wordApp.Documents.Open(SecondFilePath.Get(context), missing, readOnly,  missing, missing, missing, missing, missing, missing,
+                      missing, missing, missing, missing, missing, missing, missing);
+
+            doc = wordApp.CompareDocuments(doc1, doc2, MsWord.WdCompareDestination.wdCompareDestinationNew,
+                    MsWord.WdGranularity.wdGranularityWordLevel,
+                    //System.Boolean CompareFormatting = true,
+                    true,
+                    //System.Boolean CompareCaseChanges = true, 
+                    true,
+                    //System.Boolean CompareWhitespace = true, 
+                    true,
+                    //System.Boolean CompareTables = true, 
+                    true,
+                    //System.Boolean CompareHeaders = true,
+                    true,
+                    //System.Boolean CompareFootnotes = true,
+                    true,
+                    //System.Boolean CompareTextboxes = true, 
+                    true,
+                    //System.Boolean CompareFields = true,  
+                    true,
+                    //System.Boolean CompareComments = true,  
+                    true,
+                    //System.Boolean CompareMoves = true,
+                    true,
+                    //System.String RevisedAuthor = "",                     
+                    "",
+                    //System.Boolean IgnoreAllComparisonWarnings = false 
+                    false);
+
+            // Close first document
+            doc1.Close(missing, missing, missing);
+
+            // Close second document
+            doc2.Close(missing, missing, missing);
+
+            // Show the compared document
+            //   doc.Saved(@"D:\output\"); 
+            // doc.Save();
+            object filename = OutputPath.Get(context )+"\\Comparedoc.docx";
+            doc.SaveAs2(ref filename);
+            wordApp.Visible = true;
+        }
+    }
+}
---
+++ Eoc proofing project/New-27-05-2019/Framework/Packages/C# Code/TransposeDataTable/TransposeDataTable/TransposeDataTable.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+
+namespace TransposeDataTable
+{
+    public class TransposeDataTable : CodeActivity
+    {
+        //Red,Gereen,
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<DataTable> Transposedatatable { get; set; }
+
+        [Category("Output")]
+        public OutArgument<DataTable> Outtable { get; set; }
+        protected override void Execute(CodeActivityContext context)
+        {
+
+            DataTable inputTable = Transposedatatable.Get(context);
+            // Table shown in Figure 1.1
+
+            DataTable transposedTable = GenerateTransposedTable(inputTable);
+
+            Outtable.Set(context, transposedTable);
+
+        }
+
+        private DataTable GenerateTransposedTable(DataTable inputTable)
+        {
+            DataTable outputTable = new DataTable();
+
+            // Add columns by looping rows
+
+            // Header row's first column is same as in inputTable
+            outputTable.Columns.Add(inputTable.Columns[0].ColumnName.ToString());
+
+            // Header row's second column onwards, 'inputTable's first column taken
+            foreach (DataRow inRow in inputTable.Rows)
+            {
+                string newColName = inRow[0].ToString();
+                outputTable.Columns.Add(newColName);
+            }
+
+            // Add rows by looping columns        
+            for (int rCount = 1; rCount <= inputTable.Columns.Count - 1; rCount++)
+            {
+                DataRow newRow = outputTable.NewRow();
+
+                // First column is inputTable's Header row's second column
+                newRow[0] = inputTable.Columns[rCount].ColumnName.ToString();
+                for (int cCount = 0; cCount <= inputTable.Rows.Count - 1; cCount++)
+                {
+                    string colValue = inputTable.Rows[cCount][rCount].ToString();
+                    newRow[cCount + 1] = colValue;
+                }
+                outputTable.Rows.Add(newRow);
+            }
+
+            return outputTable;
+        }
+    }
+}
